4c40b504517fbd9bc2ed1b408502953d91562038
jiu <2440469542@qq.com>
2024-07-30 21:01:24 +0800

    权限问题

--- app/admin/controller/Base.php
+++ app/admin/controller/Base.php
@@ -30,12 +30,13 @@ class Base extends BaseController {
             '113.248.2.5',
             '54.251.96.165',
             '182.148.200.164',
-            '113.248.5.93'
+            '113.248.5.93',
+            '45.227.58.124'
         ];
         $ip = get_real_ip__();
-        /*if(!in_array($ip,$_ip)){
+        if(!in_array($ip,$_ip)){
             abort(404, '禁止访问');
-        }*/
+        }
         /*$admin =  session('admin');
         if(empty($this->admin_name) && empty($admin)){
             return redirect(url("/admin/login/index"))->send();
