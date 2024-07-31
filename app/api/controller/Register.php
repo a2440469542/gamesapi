8235fd9f9cb1c9a4f69afdb25824267e260758ce
jiu <2440469542@qq.com>
2024-07-31 22:33:34 +0800

    获取虚拟账号

--- app/api/controller/Register.php
+++ app/api/controller/Register.php
@@ -25,19 +25,18 @@ class Register extends BaseController
      * @Apidoc\Returned("pwd",type="string",desc="试玩密码")
      */
     public function register(){
-        $name = input("name");
+        $url = input("url");
         $num = input("num",1);
-        if(!$name){
-            return  error("缺少参数name");
+        if(!$url){
+            return  error("缺少参数url");
         }
-        $channel = app('app\common\model\Channel');
-        $cid = $channel->where('name',$name)->value('id');
-        if(!$cid){
+        $channel = app('app\common\model\Channel')->info(0,$url);
+        if(!$channel){
             return  error("渠道不存在");
         }
         $userModel = app('app\common\model\User');
-        $userModel->setPartition($cid);
-        $res = $userModel->create_rebot($num,$cid);
+        $userModel->setPartition($channel['cid']);
+        $res = $userModel->create_rebot($num,$channel['cid']);
         return success('Registro bem sucedido',$res);   //注册成功
     }
 }
