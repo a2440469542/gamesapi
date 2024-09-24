e0229d48344570530ae425e5b6b9f73c79611343
jiu <2440469542@qq.com>
2024-09-24 22:11:31 +0800

    注册问题

--- app/api/controller/Login.php
+++ app/api/controller/Login.php
@@ -98,6 +98,7 @@ class Login extends Base
         if($row['code'] > 0) {
             return error($row['msg'],$row['code']);   //注册失败
         }
+        print_r($row);
         $data['token'] = $row['token'];
         $data['uid'] = $row['uid'];
         Cache::set($row['token'], $data, 0); // 设置缓存，过期时间为1天
