35964f30c3d4f987b37f19bc20a73b31dcf2e097
jiu <2440469542@qq.com>
2024-12-16 15:05:14 +0800

    管理员修改余额

--- app/admin/controller/Order.php
+++ app/admin/controller/Order.php
@@ -32,7 +32,7 @@ class Order extends Base{
             $orderBy = input("orderBy", 'id desc');
             $mobile = input("mobile", '');
             $order_sn  = input("order_sn", '');
-            $cid  = input("cid", 0);
+            $cid  = input("cid", '');
             $inv_code = input("inv_code",'');
             $money = input("money",0);
             if($cid === ''){
@@ -47,7 +47,7 @@ class Order extends Base{
             if($inv_code){
                 $where[] = ['u.inv_code',"=",$inv_code];
             }
-            if($order_sn || $cid==0){
+            if($order_sn){
                 $OrderModel = app('app\common\model\Order');
             }else{
                 $OrderModel = model('app\common\model\Order',$cid);
