7f49f1de2df96142c8a50388507d4f7d074863ef
jiu <2440469542@qq.com>
2024-12-27 17:48:56 +0800

    下级充值金额

--- app/common/model/UserStat.php
+++ app/common/model/UserStat.php
@@ -330,7 +330,7 @@ class UserStat extends Base
         sum(n2_cz_money) as n2_cz_money,
         sum(n3_cz_money) as n3_cz_money';
         $user = User::alias('u')->field($filed)
-            ->leftJoin("cp_user_stat PARTITION({$this->partition}) `u`","us.uid = u.uid")
+            ->leftJoin("cp_user_stat PARTITION({$this->partition}) `us`","u.uid = us.uid")
             ->partition($this->partition)
             ->where('is_rebot',"=",0)
             ->select();
